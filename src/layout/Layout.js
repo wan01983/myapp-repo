d6f0ee02b51a22f4cb506533d463509dd6b725e6
EC2 Default User <ec2-user@ip-10-0-20-187.ap-northeast-2.compute.internal>
2022-04-15 02:08:57 +0000

    Title change

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -38,7 +38,7 @@ export default function Layout() {
                 <HomeIcon />
               </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                ExamApp
+                WYTestApp
               </Typography>
             </Toolbar>
           </AppBar>
